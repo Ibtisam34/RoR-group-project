103c5a46984a9273bc97d6bd17a790fc3187d323
unknown <Riyanagwl@gmail.com>
2023-03-27 14:10:18 +0300

    fix  linters

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,5 @@
 class Food < ApplicationRecord
-   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-   has_many :recipe_foods, dependent: :delete_all
-   has_many :recipes, through: :recipe_foods
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :recipe_foods, dependent: :delete_all
+  has_many :recipes, through: :recipe_foods
 end
